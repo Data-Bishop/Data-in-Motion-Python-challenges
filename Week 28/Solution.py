6058bccd0603ce41103b785f15deda6aec00eaae
Abasifreke Nkanang <nkanangabasifreke.data@gmail.com>
2023-02-19 08:02:58 +0100

    Updated Soluion for Week 28

--- Week 28/Solution.py
+++ Week 28/Solution.py
@@ -1,10 +1,13 @@
-def Order_List(default_list, order):
-    if order == 'none':
+def Order_List(default_list, order='Asc'):
+    order = order.capitalize()
+    if order == 'None':
         new_list = default_list
         print(new_list)
-    elif order == 'asc':
-        new_list = default_list.sort(reverse=False)
+    elif order == 'Asc':
+        new_list = sorted(default_list, reverse=False)
         print(new_list)
-    elif order == 'desc':
-        new_list = default_list.sort(reverse=True)
-        
+    elif order == 'Des':
+        new_list = sorted(default_list, reverse=True)
+        print(new_list)
+    else :
+        print("Please enter either 'none', 'asc', 'desc' as argument. Note 'asc' is set as default")
